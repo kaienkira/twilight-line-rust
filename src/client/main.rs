8aa7ff1e532b4e33d21a5599667a38d30567b7b3
kaien <kaienkira@gmail.com>
2025-03-13 22:01:07 +0800

    work sync

--- src/client/main.rs
+++ src/client/main.rs
@@ -4,6 +4,7 @@
 mod tl_client;
 
 use clap::Parser;
+use serde::Deserialize;
 
 #[derive(Parser)]
 struct Cli {
@@ -15,9 +16,34 @@ struct Cli {
 
     #[arg(short='s')]
     server_addr: Option<String>,
+
+    #[arg(short='k')]
+    sec_key: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonConfig {
+    #[serde(rename="localAddr")]
+    local_addr: Option<String>,
+
+    #[serde(rename="serverAddr")]
+    server_addr: Option<String>,
+
+    #[serde(rename="secKey")]
+    sec_key: Option<String>,
+
+    #[serde(rename="fakeRequest")]
+    fake_request: Option<Vec<String>>,
+
+    #[serde(rename="fakeResponse")]
+    fake_response: Option<Vec<String>>,
 }
 
 fn main() {
+    let opt_local_addr: Option<String> = None;
+    let opt_server_addr: Option<String> = None;
+    let opt_sec_key: Option<String> = None;
+
     let cli = Cli::parse();
 
     if let Some(config_file) = cli.config_file.as_deref() {
@@ -29,6 +55,13 @@ fn main() {
                 std::process::exit(1);
             }
         };
-        print!("{}", config_data);
+        let json_config: JsonConfig;
+        match serde_json::from_str(&config_data) {
+            Ok(v) => json_config = v,
+            Err(e) => {
+                eprintln!("parse config file {} failed: {}", config_file, e);
+                std::process::exit(1);
+            }
+        }
     }
 }
