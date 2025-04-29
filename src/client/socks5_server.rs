84cbf41907f71bd833cbb210e0a5dd6002834b59
kaien <kaienkira@gmail.com>
2025-04-29 21:47:54 +0800

    work sync

--- src/client/socks5_server.rs
+++ src/client/socks5_server.rs
@@ -17,10 +17,10 @@ impl Socks5Server {
     }
 
     pub async fn method_select(&mut self) -> Result<()> {
-        let mut buff: Vec<u8> = vec![0; 256];
+        let mut buf: Vec<u8> = vec![0; 256];
 
-        let mut b = &mut buff[..2];
-        self.conn.read_exact(&mut b).await?;
+        let b = &mut buf[..2];
+        self.conn.read_exact(b).await?;
 
         let version = b[0];
         let methods_bytes = b[1];
@@ -31,8 +31,8 @@ impl Socks5Server {
         }
 
         // discard methods
-        let mut b = &mut buff[..methods_bytes.into()];
-        self.conn.read_exact(&mut b).await?;
+        let b = &mut buf[..methods_bytes.into()];
+        self.conn.read_exact(b).await?;
 
         // answer server accepted method
         self.conn.write_all(&[0x05, 0x00]).await?;
@@ -41,10 +41,10 @@ impl Socks5Server {
     }
 
     pub async fn receive_dst_addr(&mut self) -> Result<String> {
-        let mut buff: Vec<u8> = vec![0; 256];
+        let mut buf: Vec<u8> = vec![0; 256];
 
-        let mut b = &mut buff[..4];
-        self.conn.read_exact(&mut b).await?;
+        let b = &mut buf[..4];
+        self.conn.read_exact(b).await?;
 
         let version = b[0];
         let cmd = b[1];
@@ -61,8 +61,8 @@ impl Socks5Server {
 
         if addr_type == 0x01 {
             // ipv4
-            let mut b = &mut buff[..6];
-            self.conn.read_exact(&mut b).await?;
+            let b = &mut buf[..6];
+            self.conn.read_exact(b).await?;
 
             let port: u16 = ((b[4] as u16) << 8) + b[5] as u16;
             let addr = format!("{}.{}.{}.{}:{}", b[0], b[1], b[2], b[3], port);
@@ -71,12 +71,12 @@ impl Socks5Server {
 
         } else if addr_type == 0x03 {
             // domain
-            let mut b = &mut buff[..1];
-            self.conn.read_exact(&mut b).await?;
+            let b = &mut buf[..1];
+            self.conn.read_exact(b).await?;
             let domain_length = b[0] as usize;
 
-            let mut b = &mut buff[..(domain_length + 2)];
-            self.conn.read_exact(&mut b).await?;
+            let b = &mut buf[..(domain_length + 2)];
+            self.conn.read_exact(b).await?;
             let domain = std::str::from_utf8(&b[..domain_length])?;
             let port: u16 =
                 ((b[domain_length] as u16) << 8) +
--- src/client/tl_client.rs
+++ src/client/tl_client.rs
@@ -1,3 +1,4 @@
+use bytes::BufMut;
 use cfb_mode::cipher::KeyIvInit;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
@@ -25,7 +26,8 @@ impl TlClient {
         fake_request: &'static [u8],
         fake_response: &'static [u8])
         -> TlClient {
-        let (encoder, decoder) = Self::create_cipher(sec_key.as_bytes());
+        let (encoder, decoder) =
+            Self::create_cipher(sec_key.as_bytes());
 
         TlClient {
             conn: conn,
@@ -51,15 +53,66 @@ impl TlClient {
 
     pub async fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         let n = self.conn.read(buf).await?;
-        self.decoder.decrypt(buf[..n]);
+        self.decoder.decrypt(&mut buf[..n]);
 
         Ok(n)
     }
 
-    pub async fn connect(&mut self, dst_addr: &str) -> Result<()> {
-        self.conn.write_all(self.fake_request).await?;
+    pub async fn read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
+        self.conn.read_exact(buf).await?;
+        self.decoder.decrypt(buf);
 
         Ok(())
     }
 
+    pub async fn write_all(&mut self, buf: &[u8]) -> Result<()> {
+        let mut buf_encode = buf.to_vec();
+        self.encoder.encrypt(buf_encode.as_mut_slice());
+        self.conn.write_all(buf_encode.as_slice()).await?;
+
+        Ok(())
+    }
+
+    pub async fn connect(&mut self, dst_addr: &str) -> Result<()> {
+        // write fake request
+        self.conn.write_all(self.fake_request).await?;
+
+        // write request addr
+        {
+            let mut buf: Vec<u8> = Vec::with_capacity(2048);
+            let sign: Vec<u8> = tl_common::util::sha256_sum(
+                format!("{}{}", dst_addr, self.sec_key).as_bytes());
+            buf.put_u16(dst_addr.len() as u16);
+            buf.put(dst_addr.as_bytes());
+            buf.put(sign.as_slice());
+            self.write_all(buf.as_slice()).await?;
+        }
+
+        // read fake response
+        {
+            let mut buf: Vec<u8> = vec![0; self.fake_response.len()];
+            self.conn.read_exact(buf.as_mut_slice()).await?;
+        }
+
+        // create communication key
+        {
+            let mut buf: Vec<u8> = vec![0; 256];
+
+            let b = &mut buf[..1];
+            self.read_exact(b).await?;
+            let comm_key_len = b[0] as usize;
+
+            let b = &mut buf[..comm_key_len];
+            self.read_exact(b).await?;
+            self.comm_key = b.to_vec();
+        }
+
+        // reset cipher
+        let (encoder, decoder) =
+            Self::create_cipher(self.comm_key.as_slice());
+        self.encoder = encoder;
+        self.decoder = decoder;
+
+        Ok(())
+    }
 }
